deec0537791dead13d29758b262ec505aa1132c3
o-ddong <odh0112@naver.com>
2022-10-28 18:19:01 +0900

    Feat: Wishlist 관리자 기능 추가

--- wishlists/admin.py
+++ wishlists/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from wishlists.models import Wishlist
+
 # Register your models here.
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "user",
+        "created_at",
+        "updated_at"
+    )
