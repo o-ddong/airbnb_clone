0f3156fd4cb38da664ca45099ff235177021d66c
o-ddong <odh0112@naver.com>
2022-10-28 13:04:46 +0900

    Feat: 성별, 언어, 통화 모델 추가

--- users/models.py
+++ users/models.py
@@ -1,13 +1,33 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    """
+    User Model
+    - first_name, last_name - editable=False 처리
+    """
+
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+    
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+    
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
+    avatar = models.ImageField(blank=True)
     name = models.CharField(max_length=150)
-    is_host = models.BooleanField()
+    is_host = models.BooleanField(default=True)
+    gender = models.CharField(max_length=10, choices=GenderChoices.choices)
+    language = models.CharField(max_length=2, choices=LanguageChoices.choices)
+    currency = models.CharField(max_length=5, choices=CurrencyChoices.choices)
 
     def __str__(self):
         return self.name
