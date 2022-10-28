1946cf4ac5d5c635169215aa6ccb72fcdf9d183e
o-ddong <odh0112@naver.com>
2022-10-28 16:47:58 +0900

    Feat: Experience, Perk 관리자 기능 추가

--- experiences/admin.py
+++ experiences/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from experiences.models import Experience, Perk
+
 # Register your models here.
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "price", "start", "end",
+    )
+
+@admin.register(Perk)
+class PerkAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "details", "explanation"
+    )
