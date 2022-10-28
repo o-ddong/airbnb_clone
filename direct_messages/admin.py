6a46d729ea33ab8c08e336a60da076dee685fae5
o-ddong <odh0112@naver.com>
2022-10-28 19:20:49 +0900

    Feat: ChattingRoom, Message 관리자 기능 생성

--- direct_messages/admin.py
+++ direct_messages/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from direct_messages.models import Message, ChattingRoom
+
 # Register your models here.
+@admin.register(ChattingRoom)
+class ChattingRoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "created_at",
+    )
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        "text",
+        "user",
+        "room",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+    )
