1b84c2b10407a3ce15ec592da55b07dd2ca6fda1
o-ddong <odh0112@naver.com>
2022-10-28 16:08:49 +0900

    Feat: Room, Amenity 관리자 기능 추가

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from rooms.models import Amenity, Room
+
 # Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "kind", "owner", "created_at", "updated_at")
+    list_filter = ("city", "price", "rooms", "toilets", "pet_friendly", "kind", "amenities")
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "description", "created_at", "updated_at")
