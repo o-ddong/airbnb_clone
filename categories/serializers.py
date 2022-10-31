6ab8a22ee151021f0de74841b8351970a2b45453
o-ddong <odh0112@naver.com>
2022-10-31 17:46:42 +0900

    Refactor: DRF 이용해 view, serializer, url 구조화 진행

--- categories/serializers.py
+++ categories/serializers.py
@@ -3,19 +3,7 @@ from rest_framework import serializers
 from categories.models import Category
 
 
-class CategorySerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=True)
-    kind = serializers.ChoiceField(
-        choices=Category.CategoryKindChoices.choices,
-    )
-    created_at = serializers.DateField(read_only=True)
-
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.kind = validated_data.get("kind", instance.kind)
-        instance.save()
-        return instance
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
--- categories/urls.py
+++ categories/urls.py
@@ -3,6 +3,17 @@ from django.urls import path
 from categories import views
 
 urlpatterns = [
-    path("", views.Categories.as_view()),
-    path("<int:pk>", views.CategoryDetail.as_view()),
+    path("", views.CategoryViewset.as_view(
+        {
+            "get": "list",
+            "post": "create",
+        }
+    )),
+    path("<int:pk>", views.CategoryViewset.as_view(
+        {
+            "get": "retrieve",
+            "put": "partial_update",
+            "delete": "destroy",
+        }
+    )),
 ]
