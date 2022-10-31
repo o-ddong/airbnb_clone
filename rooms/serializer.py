d336b7af5667b01017c1b4b740de1aba8c06847c
o-ddong <odh0112@naver.com>
2022-10-31 23:09:04 +0900

    Feat: Room list, detail Serializer 생성

--- rooms/serializer.py
+++ rooms/serializer.py
@@ -1,9 +1,30 @@
 from rest_framework.serializers import ModelSerializer
 
-from rooms.models import Amenity
+from categories.serializers import CategorySerializer
+from rooms.models import Amenity, Room
+from users.serializers import TinyUserSerializer
 
 
 class AmenitySerializer(ModelSerializer):
     class Meta:
         model = Amenity
+        fields = ("name", "description")
+
+
+class RoomDetailSerializer(ModelSerializer):
+
+    owner = TinyUserSerializer()
+    amenities = AmenitySerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = Room
         fields = "__all__"
+
+
+class RoomListSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = (
+            "pk", "name", "country", "city", "price",
+        )
