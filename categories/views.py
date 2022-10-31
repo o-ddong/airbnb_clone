1d972d54826c04567d2f7a3b542dffd6a3d6b6b4
o-ddong <odh0112@naver.com>
2022-10-31 16:13:52 +0900

    Feat: category APIView 생성

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
+
+from categories.models import Category
+from categories.serializers import CategorySerializer
+
+
+class Categories(APIView):
+    def get(self, request):
+        print("=====================GET=====================")
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        print("=====================POST=====================")
+        serializer = CategorySerializer(data=request.data)
+        print(serializer.is_valid())
+        if serializer.is_valid():
+            new_category = serializer.save()
+            return Response(serializer.errors)
+
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            print("Not Found")
+            raise NotFound
+
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = CategorySerializer(
+            self.get_object(pk),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
