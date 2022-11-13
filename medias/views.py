b279e4f0ba01285f226c49d83588c7a258b16a73
o-ddong <odh0112@naver.com>
2022-11-13 12:19:34 +0900

    Feat: Photo in Views 삭제 기능 추가

--- medias/views.py
+++ medias/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+
+from .models import Photo
 
 # Create your views here.
+class PhotoDetail(APIView):
+    def get_objects(pk):
+        try:
+            return Photo.objects.get(pk=pk)
+        except Photo.DoesNotExist:
+            
+
+    def delete(self, request, pk):
+        pass
