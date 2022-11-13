61d293014a02c960edec0e6da69654ea2bddcbd0
o-ddong <odh0112@naver.com>
2022-11-13 11:49:09 +0900

    Fix: 로컬 -> 클라우드 업로드 변경 위한 모델 수정

--- medias/models.py
+++ medias/models.py
@@ -6,7 +6,7 @@ from common.models import CommonModel
 class Photo(CommonModel):
     """ Photo Model Definition """
 
-    file = models.ImageField()
+    file = models.URLField()
     description = models.CharField(max_length=140)
     room = models.ForeignKey(
         "rooms.Room",
@@ -30,7 +30,7 @@ class Photo(CommonModel):
 class Video(CommonModel):
     """ Video Model Definition """
 
-    file = models.FileField()
+    file = models.URLField()
     experience = models.OneToOneField(
         "experiences.Experience",
         on_delete=models.CASCADE,
