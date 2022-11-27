42bf51eda5e77f031aad0a37a8d0a8d831d298f8
o-ddong <odh0112@naver.com>
2022-11-27 17:49:31 +0900

    Feat: Wishlist Detail(get, put, delete), Toggle(추가, 삭제) 기능 추가

--- wishlists/serializers.py
+++ wishlists/serializers.py
@@ -1,10 +1,15 @@
-from rest_framework import ModelSerializer
+from rest_framework.serializers import ModelSerializer
+from rooms.serializer import RoomListSerializer
 
 from wishlists.models import Wishlist
 
 
 class WishlistSerializer(ModelSerializer):
 
+    rooms = RoomListSerializer(many=True, read_only=True)
+
     class Meta:
         model = Wishlist
-        fields = "__all__"
+        fields = (
+            "pk", "name", "rooms",
+        )
