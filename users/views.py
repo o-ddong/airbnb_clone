4998c19d1cf7dbaadbb29927e9fa3dabe92ba6cb
odh0112 <odh0112@naver.com>
2022-12-02 12:49:20 +0900

    Feat: User Password 변경 로직 추가

--- users/views.py
+++ users/views.py
@@ -60,3 +60,21 @@ class PublicUser(APIView):
             raise NotFound
         serializer = serializers.PrivateUserSerializer(user)
         return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
+        if not old_password or not new_password:
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
